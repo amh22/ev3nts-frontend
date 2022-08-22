10752aace0c7f55da83cfddc600f260d2e3c0d8c
Andrew Henry <andrewmhenry@me.com>
2022-08-22 20:08:58 +1000

    🚀 feat(Event Details): implemented subgraph query on creation of new event for the event details page

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -1,8 +1,51 @@
 import Head from 'next/head'
+import { gql } from '@apollo/client'
+import client from '../../apollo-client'
 
 import { EmojiHappyIcon, TicketIcon, UsersIcon, LinkIcon } from '@heroicons/react/outline'
 
-function Event() {
+export async function getServerSideProps(context) {
+  const { id } = context.params
+
+  const { data } = await client.query({
+    query: gql`
+      query Event($id: String!) {
+        event(id: $id) {
+          id
+          eventID
+          name
+          description
+          link
+          eventOwner
+          eventTimestamp
+          maxCapacity
+          deposit
+          totalRSVPs
+          totalConfirmedAttendees
+          imageURL
+          rsvps {
+            id
+            attendee {
+              id
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      id: id,
+    },
+  })
+
+  return {
+    props: {
+      event: data.event,
+    },
+  }
+}
+
+function Event({ event }) {
+  console.log('EVENT:', event)
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
       <Head>
@@ -44,5 +87,3 @@ function Event() {
 }
 
 export default Event
-
-export async function getServerSideProps() {}
